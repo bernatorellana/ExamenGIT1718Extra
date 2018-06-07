7d34f29e955ba33edf38978402a2cb3612fedb93
Dykmor <m2.dperez@infomila.info>
2018-06-07 18:57:49 +0200

    final

--- src/Fireworks/Random.java
+++ src/Fireworks/Random.java
@@ -9,5 +9,6 @@ class Random {
 
 	   public static void main(String[] args) {
                System.out.println("it's me mario");
+               System.out.println("No em donara temps AAAAAA");
     }
 }
